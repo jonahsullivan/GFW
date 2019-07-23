3849ca434cc797daeda08018b92b6230fa0a358f
jonah.sullivan <jonah.sullivan@ga.gov.au>
2019-07-23 18:28:36 +1000

    added restart functionality

--- LoadGFW.py
+++ LoadGFW.py
@@ -1,6 +1,7 @@
 import psycopg2
 import os
 import csv
+import datetime
 
 inFolder = r'/home/jonah/Desktop/fishing_effort/daily_csvs'
 
@@ -54,37 +55,54 @@ if create_table:
     cur.close()
     conn.commit()
 
+# get a list with all of the csv dates that have been loaded
+print("getting dates already loaded")
+get_dates_command = """
+SELECT DISTINCT date FROM public.effort;
+"""
+cur = conn.cursor()
+cur.execute(get_dates_command)
+dates = cur.fetchall()
+cur.close()
+print("found " + str(len(dates)) + " dates")
+
 # loop through csvs adding data
 cur = conn.cursor()
 for f in file_list:
-    print(f)
-    entries = []
-    with open(f, 'r') as csvfile:
-        csvfile.readline()  # skip header
-        reader = csv.reader(csvfile)
-        for row in reader:
-            lat = float(row[1]) / 100
-            if lat_max1 > lat > lat_min1 or lat_max2 > lat > lat_min2:
-                lon = float(row[2]) / 100
-                if lon_max1 > lon > lon_min1 or lon_max2 > lon > lon_min2:
-                    entries.append(row)
-    print("found " + str(len(entries)) + " rows")
-    for entry in entries:
-        lon = float(entry[2]) / 100
-        lat = float(entry[1]) / 100
-        del entry[1]
-        del entry[1]
-        coordinates = "POINT(%s %s)" % (lon, lat)
-        entry.append(coordinates)
-        try:
-            cur.execute("""
-                        INSERT INTO effort (date, flag, geartype, vessel_hours, fishing_hours, mmsi_present, geom)
-                        VALUES (%s, %s, %s, %s, %s, %s, ST_GeomFromText(%s, 4326));
-                        """,
-                        entry)
-        except psycopg2.IntegrityError:
-            conn.rollback()
-        else:
-            conn.commit()
+    d = os.path.basename(f)
+    d = datetime.date(int(d[0:4]), int(d[5:7]), int(d[8:10]))
+    print(d)
+    if d in dates:
+        print("skipping " + d)
+    else:
+        print(f)
+        entries = []
+        with open(f, 'r') as csvfile:
+            csvfile.readline()  # skip header
+            reader = csv.reader(csvfile)
+            for row in reader:
+                lat = float(row[1]) / 100
+                if lat_max1 > lat > lat_min1 or lat_max2 > lat > lat_min2:
+                    lon = float(row[2]) / 100
+                    if lon_max1 > lon > lon_min1 or lon_max2 > lon > lon_min2:
+                        entries.append(row)
+        print("found " + str(len(entries)) + " rows")
+        for entry in entries:
+            lon = float(entry[2]) / 100
+            lat = float(entry[1]) / 100
+            del entry[1]
+            del entry[1]
+            coordinates = "POINT(%s %s)" % (lon, lat)
+            entry.append(coordinates)
+            try:
+                cur.execute("""
+                            INSERT INTO effort (date, flag, geartype, vessel_hours, fishing_hours, mmsi_present, geom)
+                            VALUES (%s, %s, %s, %s, %s, %s, ST_GeomFromText(%s, 4326));
+                            """,
+                            entry)
+            except psycopg2.IntegrityError:
+                conn.rollback()
+            else:
+                conn.commit()
 cur.close()
 conn.close()
